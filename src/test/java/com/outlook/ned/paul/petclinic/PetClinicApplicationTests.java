3984a5ddee6a247e76884b3200f521dc0651927b
paul <paul.ned@outlook.com>
2020-09-18 20:22:31 +0200

    Add initial project

---
+++ src/test/java/com/outlook/ned/paul/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outlook.ned.paul.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
