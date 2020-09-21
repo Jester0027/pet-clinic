7621dfa29f513786396276ae92758f6ebbb81e51
paul <paul.ned@outlook.com>
2020-09-21 10:03:57 +0200

    Implemented CrudService, closes #11

--- pet-clinic-data/src/main/java/com/outlook/ned/paul/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/outlook/ned/paul/petclinic/services/PetService.java
@@ -2,13 +2,5 @@ package com.outlook.ned.paul.petclinic.services;
 
 import com.outlook.ned.paul.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
