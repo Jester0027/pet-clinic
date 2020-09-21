3a3d4bfd1e36460f08e046f3e27ee3d85e76c4df
paul <paul.ned@outlook.com>
2020-09-21 12:30:07 +0200

    Added Owner index page and controller, closes #13

--- pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/controllers/OwnerController.java
@@ -3,12 +3,12 @@ package com.outlook.ned.paul.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/pets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"/", ""})
     public String index() {
-        return "vets/index";
+        return "owners/index";
     }
 }
