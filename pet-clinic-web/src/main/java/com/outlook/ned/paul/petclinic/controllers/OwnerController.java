6ba997ac1b349c5008f3882690930af2fad66e86
paul <paul.ned@outlook.com>
2020-09-22 11:25:04 +0200

    List owners on owners page, closes #16

--- pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package com.outlook.ned.paul.petclinic.controllers;
 
+import com.outlook.ned.paul.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/", ""})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
