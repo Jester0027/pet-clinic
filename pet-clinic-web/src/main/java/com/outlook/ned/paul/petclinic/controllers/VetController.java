4f6cd589166109071eebd9b4c25e68835c02cf42
paul <paul.ned@outlook.com>
2020-09-22 11:36:20 +0200

    List vets on vets page, closes #15

--- pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.outlook.ned.paul.petclinic.controllers;
 
+import com.outlook.ned.paul.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/pets")
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/", ""})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
