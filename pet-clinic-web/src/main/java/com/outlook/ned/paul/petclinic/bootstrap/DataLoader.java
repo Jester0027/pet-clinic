a6d757c25cd8897e5e13b0658b82c4f94c844e5c
paul <paul.ned@outlook.com>
2020-09-25 18:13:59 +0200

    Added pet types data on startup, closes #32

--- pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.outlook.ned.paul.petclinic.bootstrap;
 
 import com.outlook.ned.paul.petclinic.model.Owner;
+import com.outlook.ned.paul.petclinic.model.PetType;
 import com.outlook.ned.paul.petclinic.model.Vet;
 import com.outlook.ned.paul.petclinic.services.OwnerService;
+import com.outlook.ned.paul.petclinic.services.PetTypeService;
 import com.outlook.ned.paul.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         for (int i = 0; i < 3; i++) {
             Owner owner = new Owner();
             owner.setFirstName("owner");
