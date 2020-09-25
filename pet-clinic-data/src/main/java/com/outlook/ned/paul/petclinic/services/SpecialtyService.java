0e1280aa302f8c9dbc8ce5923ed186a4be88b605
paul <paul.ned@outlook.com>
2020-09-25 21:20:01 +0200

    Added specialty service map, closes #31

---
+++ pet-clinic-data/src/main/java/com/outlook/ned/paul/petclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.outlook.ned.paul.petclinic.services;
+
+import com.outlook.ned.paul.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
