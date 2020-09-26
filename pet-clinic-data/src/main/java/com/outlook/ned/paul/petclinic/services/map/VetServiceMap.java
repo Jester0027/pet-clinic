536560a92d216f9481a95904dc8afc8c0dd29dbb
paul <paul.ned@outlook.com>
2020-09-26 09:13:16 +0200

    Added specialties data on startup, closes #34

--- pet-clinic-data/src/main/java/com/outlook/ned/paul/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/outlook/ned/paul/petclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.outlook.ned.paul.petclinic.services.map;
 
+import com.outlook.ned.paul.petclinic.model.Specialty;
 import com.outlook.ned.paul.petclinic.model.Vet;
+import com.outlook.ned.paul.petclinic.services.SpecialtyService;
 import com.outlook.ned.paul.petclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,13 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -25,6 +34,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialties().size() > 0) {
+            object.getSpecialties().forEach(s -> {
+                if (s.getId() == null) {
+                    Specialty savedSpecialty = specialtyService.save(s);
+                    s.setId(savedSpecialty.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/outlook/ned/paul/petclinic/bootstrap/DataLoader.java
@@ -1,13 +1,7 @@
 package com.outlook.ned.paul.petclinic.bootstrap;
 
-import com.outlook.ned.paul.petclinic.model.Owner;
-import com.outlook.ned.paul.petclinic.model.Pet;
-import com.outlook.ned.paul.petclinic.model.PetType;
-import com.outlook.ned.paul.petclinic.model.Vet;
-import com.outlook.ned.paul.petclinic.services.OwnerService;
-import com.outlook.ned.paul.petclinic.services.PetService;
-import com.outlook.ned.paul.petclinic.services.PetTypeService;
-import com.outlook.ned.paul.petclinic.services.VetService;
+import com.outlook.ned.paul.petclinic.model.*;
+import com.outlook.ned.paul.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -20,17 +14,32 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final PetService petService;
+    private final SpecialtyService specialtyService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, PetService petService) {
+    public DataLoader(
+            OwnerService ownerService,
+            VetService vetService,
+            PetTypeService petTypeService,
+            PetService petService,
+            SpecialtyService specialtyService
+    ) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.petService = petService;
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        int count = petTypeService.findAll().size();
 
+        if (count == 0) {
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -39,14 +48,27 @@ public class DataLoader implements CommandLineRunner {
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
+        Specialty radiology = new Specialty();
+        radiology.setDescription("Radiology");
+        Specialty savedRadiology = specialtyService.save(radiology);
+
+        Specialty surgery = new Specialty();
+        surgery.setDescription("Surgery");
+        Specialty savedSurgery = specialtyService.save(surgery);
+
+        Specialty dentistry = new Specialty();
+        dentistry.setDescription("Dentistry");
+        Specialty savedDentistry = specialtyService.save(dentistry);
+
+
         for (int i = 0; i < 3; i++) {
 
             Owner owner = new Owner();
             owner.setFirstName("owner");
-            owner.setLastName(""+i);
+            owner.setLastName("" + i);
             owner.setAddress("Address " + (i + 1));
             owner.setCity("City");
-            owner.setTelephone("+" + (i + 1) +"23 45 67 89");
+            owner.setTelephone("+" + (i + 1) + "23 45 67 89");
             ownerService.save(owner);
 
             Pet pet = new Pet();
@@ -59,7 +81,13 @@ public class DataLoader implements CommandLineRunner {
 
             Vet vet = new Vet();
             vet.setFirstName("Vet");
-            vet.setLastName(""+i);
+            vet.setLastName("" + i);
+            vet.getSpecialties().add(Math.random() > 0.66
+                    ? savedDentistry
+                    : Math.random() > 0.5
+                    ? savedRadiology
+                    : savedSurgery
+            );
             vetService.save(vet);
         }
 
@@ -67,7 +95,7 @@ public class DataLoader implements CommandLineRunner {
         for (long i = 0; i < 3; i++) {
             Vet vet = vetService.findById(i);
             System.out.println("---");
-            System.out.println(vet.getFirstName() + " " + vet.getLastName());
+            System.out.println(vet.getFirstName() + " " + vet.getLastName() + " --- " + vet.getSpecialties());
         }
     }
 }
